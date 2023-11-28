a70bfb8919c4a8767bf907d806ce4dc8c162fe7e
Chinecherem <chinecheremubawike@gmail.com>
2023-11-28 10:21:41 +0100

    New changes

--- script.js
+++ script.js
@@ -1,144 +1,9 @@
-// Function to toggle steps
-const displayContent = (stepNumber) => {
-  let content = document.getElementById('content' + stepNumber);
-  let selectedStep = document.querySelector('.step:nth-child(' + stepNumber + ')');
-
-  // Check if the clicked content is already open
-  if (content.style.display === 'block') {
-      return; // Do nothing if the content is already open
-  }
-
-  // Hide all other contents before displaying the selected one
-  let contents = document.querySelectorAll('.step_content');
-  contents.forEach(function(stepContent) {
-      stepContent.style.display = 'none';
-  });
-
-  // Hide all step images before displaying the selected one
-  let stepImages = document.querySelectorAll('.step_image');
-  stepImages.forEach(function(image) {
-      image.style.display = 'none';
-  });
-
-  // Display the clicked content
-  content.style.display = 'block';
-
-  // Display the image for the clicked step
-  let selectedStepImage = selectedStep.querySelector('.step_image');
-    // selectedStepImage.style.display = 'block';
-    selectedStepImage.classList.toggle('.step_image_active')
-
-
-  // Remove active class from all items
-  let allSteps = document.querySelectorAll('.step');
-  allSteps.forEach(function(step) {
-      step.classList.remove('step_active');
-  });
-
-  // Add active class to the clicked item
-  selectedStep.classList.add('step_active');
-}
-
-// function toggleCheckbox(element) {
-//     // Find the unchecked and checked SVG elements within the clicked .step div
-//     const uncheckedCheckbox = element.querySelector('#step_checkbox_unchecked');
-//     const checkedCheckbox = element.querySelector('#step_checkbox_checked');
-
-//     // Toggle the display property of the checkboxes
-//     if (uncheckedCheckbox.style.display === 'block') {
-//         uncheckedCheckbox.style.display = 'none';
-//         checkedCheckbox.style.display = 'block';
-//     } else {
-//         uncheckedCheckbox.style.display = 'block';
-//         checkedCheckbox.style.display = 'none';
-//     }
-// }
-
-
-// Code for checkboxes starts here
-
-const checkboxBtn = document.querySelector('.step_checkbox');
-const checkboxes = document.querySelectorAll('.step_checkbox');
-const progressBar = document.querySelector('.progress_bar');
-const progressText = document.querySelector('.progress_bar_number');
-
-const checkboxButtonStatus = document.querySelector('#shopping-item-checkbox-status')
-
-// Initialize the count of checked checkboxes
-let checkedCount = 0;
-
-checkboxes.forEach(checkbox => {
-  checkbox.addEventListener('click', function() {
-    const isChecked = this.getAttribute('data-checked') === 'true';
-    this.setAttribute('data-checked', !isChecked);
-
-    const checkedSVG = this.querySelector('#step_checkbox_checked');
-    const uncheckedSVG = this.querySelector('#step_checkbox_unchecked');
-    const transitionSVG = this.querySelector('#step_checkbox_transition');
-
-
-    if (isChecked) {
-      checkedSVG.style.display = 'none';
-      transitionSVG.style.display = 'block';
-
-      checkboxButtonStatus.ariaLabel = 'Loading. Please wait...';
-
-      setTimeout(()=>{
-        transitionSVG.style.display = 'none';
-        uncheckedSVG.style.display = 'block';
-        checkboxBtn.ariaLabel = checkboxBtn.ariaLabel.replace('as not done', 'as done');
-
-        checkboxButtonStatus.ariaLabel = 'Successfully marked clean your room as not done';
-
-        }, 2000);
-
-      checkedCount--;
-    } else {
-      uncheckedSVG.style.display = 'none';
-      transitionSVG.style.display = 'block';
-
-      checkboxButtonStatus.ariaLabel = 'Loading. Please wait...';
-
-      setTimeout(()=>{
-        transitionSVG.style.display = 'none';
-        checkedSVG.style.display = 'block';
-        checkboxBtn.ariaLabel = checkboxBtn.ariaLabel.replace('as done', 'as not done');
-
-        checkboxButtonStatus.ariaLabel = 'Successfully marked clean your room as done'
-      }, 2000)
-
-      checkedCount++;
-     
-    }
-
-    updateProgressBar(checkedCount);
-  });
-
-  // Check initial state and count checked checkboxes
-  if (checkbox.getAttribute('data-checked') === 'true') {
-    checkedCount++;
-  }
-});
-
-// Set initial progress text content
-progressText.textContent = `${checkedCount} / ${checkboxes.length} completed`;
-
-function updateProgressBar(checkedCount) {
-  const total = checkboxes.length;
-  const progressPercentage = (checkedCount / total) * 100;
-  progressBar.style.width = progressPercentage + '%';
-  progressText.textContent = `${checkedCount} / ${total} completed`;
-}
-
-// Code for checkboxes ends here
-
-function App (){
+// Code for profile menu display starts here
+function triggerProfileMenu(){
     // Get menu trigger
     const menuTrigger = document.getElementById('store_nav_details');
-
     //get menu
     const menu = document.getElementById("profile_menu_items");
-
     // Get all menus items
     const allMenuItems = menu.querySelectorAll('[role = "menuitem"]');
 
@@ -225,10 +90,13 @@ function App (){
     //Add event listener to menu trigger, and call toggleMenu function when the "click" event happens
     menuTrigger.addEventListener('click', toggleMenu);
 
+}
+triggerProfileMenu();
 
+// Code for notification icon menu display starts here
+function triggerNotification(){
     // Get Notification Trigger
     const notificationTrigger = document.getElementById('notification_bell')
-
     //get Notification menu
     const notificationMenu = document.getElementById("notification_menu");
 
@@ -248,9 +116,9 @@ function App (){
     function openNotificationMenu (){
         notificationTrigger.ariaExpanded = "true";
         // notificationMenu.focus();
-        const checkBtn = document.getElementById('check');
-        checkBtn.focus();
-        notificationMenu.addEventListener('keyup', handleNotificationMenuEscapeKeypress);
+        // const checkBtn = document.getElementById('check');
+        // checkBtn.focus();
+        // notificationMenu.addEventListener('keyup', handleNotificationMenuEscapeKeypress);
 
     }
 
@@ -270,8 +138,10 @@ function App (){
 
     //Add event listener to notifcation trigger
     notificationTrigger.addEventListener('click', toggleNotification);
+}
+triggerNotification();
 
-
+function dismissTrialCallout (){  
     // Get trial callout div
     const trialCallout =  document.getElementById('trial_callout');
 
@@ -282,16 +152,16 @@ function App (){
     trialCalloutDismiss.addEventListener('click', function(){
         trialCallout.classList.toggle('select_plan_dismiss')
     })
+}
+dismissTrialCallout();
 
+function toggleSetupguide(){
     // Get the steps container
     const stepsContainer = document.getElementById('step_container');
-
     //Get the arrow up icon
     const arrowUp = document.getElementById('setup_uparrow');
-
      //Get the arrow down icon
     const arrowDown = document.getElementById('setup_downarrow');
-
     // Add event listener to the arrow up icon
 
     arrowUp.addEventListener('click', function(){
@@ -305,16 +175,154 @@ function App (){
         arrowUp.style.display = 'block';
         stepsContainer.classList.toggle('step_container_close')
     })
+}
+toggleSetupguide();
 
+// Function to toggle steps and check step checkboxes
+function toggleSteps(){
+    // Code for toggling steps/step content starts here
 
+    // Function to toggle steps and display steps content
+    function displayContent(stepNumber){
+        let content = document.getElementById('content' + stepNumber);
+        let selectedStep = document.querySelector('.step:nth-child(' + stepNumber + ')');
+
+        // Check if the clicked content is already open
+        if (content.style.display === 'block') {
+            return; // Do nothing if the content is already open
+        }
+      
+        // Hide all other contents before displaying the selected one
+        let contents = document.querySelectorAll('.step_content');
+        contents.forEach(function(stepContent) {
+            stepContent.style.display = 'none';
+        });
+      
+        // Hide all step images before displaying the selected one
+        let stepImages = document.querySelectorAll('.step_image');
+        stepImages.forEach(function(image) {
+            image.style.display = 'none';
+        });
+      
+        // Display the clicked content
+        content.style.display = 'block';
+      
+        // Display the image for the clicked step
+        let selectedStepImage = selectedStep.querySelector('.step_image');
+        // selectedStepImage.style.display = 'block';
+        selectedStepImage.classList.toggle('.step_image_active')
+      
+        // Remove active class from all items
+        let allSteps = document.querySelectorAll('.step');
+        allSteps.forEach(function(step) {
+            step.classList.remove('step_active');
+        });
+      
+        // Add active class to the clicked item
+        selectedStep.classList.add('step_active');
+    }
+    // Get all steps
+    const steps = document.querySelectorAll('.step');
+    steps.forEach((step, index)=>{
+        step.addEventListener('click', (event)=>{
+        displayContent(event, index + 1)
+        })
+    })
+
+    // Code for toggling step checkboxes starts here
+
+     // Initialize the count of checked checkboxes
+     let checkedCount = 0;
+     const checkboxBtn = document.querySelector('.step_checkbox');
+     const checkboxes = document.querySelectorAll('.step_checkbox');
+     const progressBar = document.querySelector('.progress_bar');
+     const progressText = document.querySelector('.progress_bar_number');
+     const checkboxButtonStatus = document.querySelector('#shopping-item-checkbox-status');   
+
+     function toggleCheckboxOpen(checkedSVG, uncheckedSVG, transitionSVG, index){
+        uncheckedSVG.style.display = 'none';
+        transitionSVG.style.display = 'block';
+
+        checkboxButtonStatus.ariaLabel = 'Loading. Please wait...';
+
+        setTimeout(()=>{
+            transitionSVG.style.display = 'none';
+            checkedSVG.style.display = 'block';
+            checkboxBtn.ariaLabel = checkboxBtn.ariaLabel.replace('as done', 'as not done');
+
+            checkboxButtonStatus.ariaLabel = 'Successfully marked clean your room as done'
+        }, 2000)
+
+        checkedCount++;
+        updateProgressBar(checkedCount);
+        // Loop through each 'step' element
+        for (let i = index; i < steps.length; i++) {
+            const step = steps[i];
+            
+            // Find the button inside each 'step' element
+            const button = step.querySelector('.step_checkbox');
+            
+            // Check if the button has data-checked="false"
+            if (button && button.getAttribute('data-checked') === 'false') {
+            console.log(`Button with data-checked="false" found in step ${i + 1}`);
+            // Perform actions or store information about the specific step here
+            displayContent(i + 1);
+            return
+            }
+        }
+     }
+
+     function toggleCheckboxClose(checkedSVG, uncheckedSVG, transitionSVG){
+        checkedSVG.style.display = 'none';
+        transitionSVG.style.display = 'block';
+
+        checkboxButtonStatus.ariaLabel = 'Loading. Please wait...';
+
+        setTimeout(()=>{
+            transitionSVG.style.display = 'none';
+            uncheckedSVG.style.display = 'block';
+            checkboxBtn.ariaLabel = checkboxBtn.ariaLabel.replace('as not done', 'as done');
+
+            checkboxButtonStatus.ariaLabel = 'Successfully marked clean your room as not done';
+
+            }, 2000);
     
+        checkedCount--;
+     }
+    
+    checkboxes.forEach((checkbox, index) => {
+        checkbox.addEventListener('click', function(event) {
+            event.stopPropagation(); // Prevents the click event from bubbling up to the parent div
+            const isChecked = this.getAttribute('data-checked') === 'true';
+            this.setAttribute('data-checked', !isChecked);
+            const checkedSVG = this.querySelector('#step_checkbox_checked');
+            const uncheckedSVG = this.querySelector('#step_checkbox_unchecked');
+            const transitionSVG = this.querySelector('#step_checkbox_transition');
+    
+            if (isChecked) {
+                toggleCheckboxClose(checkedSVG, uncheckedSVG, transitionSVG)
+            } else {
+                toggleCheckboxOpen(checkedSVG, uncheckedSVG, transitionSVG, index);
+            }
+    
+            updateProgressBar(checkedCount);
+        });
 
+        // Check initial state and count checked checkboxes
+        if (checkbox.getAttribute('data-checked') === 'true') {
+            checkedCount++;
+        }
+    });
+
+    // Set initial progress text content
+    progressText.textContent = `${checkedCount} / ${checkboxes.length} completed`;
+
+    function updateProgressBar(checkedCount) {
+        const total = checkboxes.length;
+        const progressPercentage = (checkedCount / total) * 100;
+        progressBar.style.width = progressPercentage + '%';
+        progressText.textContent = `${checkedCount} / ${total} completed`;
+    }
 
 }
-
-App();
-
-function displayMenuItems(){
-  
-}
-
+toggleSteps()
